3af2f1c54273b69b8c5eb12fbfa7442ab9a2fcc5
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-08-01 19:58:02 -0300

    feat: added cors

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@
 import express from 'express'
 import db from './database.js'
 import routes from './routes.js'
+import cors from 'cors'
 
 const app = express()
 
 const port = 3000
 
+app.use(cors())
 app.use(express.json())
 db.connect()
 app.use(routes)
